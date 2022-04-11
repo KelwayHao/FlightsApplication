d1e8047621c981313f5217b06ff8569fa097203d
KelwayHao <kelwaycf@gmail.com>
2022-04-11 13:02:43 +0300

    Correction 16 ViewBinding

--- app/src/main/java/com/example/flightsapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/flightsapplication/presentation/MainActivity.kt
@@ -9,12 +9,9 @@ import com.example.flightsapplication.utils.openFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 
     override fun onStart() {
--- app/src/main/java/com/example/flightsapplication/presentation/recycler/FlightViewHolder.kt
+++ app/src/main/java/com/example/flightsapplication/presentation/recycler/FlightViewHolder.kt
@@ -2,43 +2,35 @@ package com.example.flightsapplication.presentation.recycler
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import by.kirich1409.viewbindingdelegate.viewBinding
-import com.example.flightsapplication.R
 import com.example.flightsapplication.databinding.ItemFlightHistoryBinding
 import com.example.flightsapplication.domain.models.FlightTicket
 import com.example.flightsapplication.utils.getValuePassengerAge
 
-class FlightViewHolder(itemView: View) :
-    RecyclerView.ViewHolder(itemView) {
-
-    private val binding by viewBinding<ItemFlightHistoryBinding>()
+class FlightViewHolder(private val binding: ItemFlightHistoryBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     companion object {
-        fun newInstance(parent: ViewGroup) =
-            FlightViewHolder(
-                LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_flight_history, parent, false)
+        fun newInstance(parent: ViewGroup) = FlightViewHolder(
+            ItemFlightHistoryBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
             )
+        )
     }
 
     @SuppressLint("SetTextI18n")
     fun bindItem(flightTicket: FlightTicket) {
-        with(binding) {
-            with(itemView) {
-                with(flightTicket) {
-                    historyDeparture.text = departure
-                    historyDestination.text = destination
-                    historyDateDepart.text = departDate
-                    historyDateArrival.text = returnDate
-                    historyNumberPassportPassenger.text = numberPassportPassenger
-                    historyNamePassenger.text = namePassenger
-                    historyPassengerAge.text =
-                        context.getString(passengerAge.idAge.getValuePassengerAge())
-                }
-            }
+        with(flightTicket) {
+            binding.historyDeparture.text = departure
+            binding.historyDestination.text = destination
+            binding.historyDateDepart.text = departDate
+            binding.historyDateArrival.text = returnDate
+            binding.historyNumberPassportPassenger.text = numberPassportPassenger
+            binding.historyNamePassenger.text = namePassenger
+            binding.historyPassengerAge.text = binding.root.context.getString(passengerAge.idAge.getValuePassengerAge())
         }
     }
 }
